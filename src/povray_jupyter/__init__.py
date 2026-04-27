de0898f0a72624cdd08a96a7cc23222f5938e971
Luna <itsactuallyluna9@gmail.com>
2026-04-27 10:20:47 -0500

    export display_png from main package

--- src/povray_jupyter/__init__.py
+++ src/povray_jupyter/__init__.py
@@ -1,4 +1,4 @@
-from .magic import PovRayMagics
+from .magic import PovRayMagics, display_png
 from .render import render
 
 
@@ -6,4 +6,4 @@ def load_ipython_extension(ipython):
     ipython.register_magics(PovRayMagics)
 
 
-__all__ = ["render"]
+__all__ = ["render", "display_png"]
